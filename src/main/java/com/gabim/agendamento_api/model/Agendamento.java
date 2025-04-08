892023377496a08bf9d8eda23a54345ea24513b4
gabimmdev <gabrielagomescezar@gmail.com>
2025-04-08 12:09:55 -0300

    feat: agendamento

--- src/main/java/com/gabim/agendamento_api/model/Agendamento.java
+++ src/main/java/com/gabim/agendamento_api/model/Agendamento.java
@@ -1,5 +1,52 @@
 package com.gabim.agendamento_api.model;
 
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
 public class Agendamento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String cliente;
+
+    private LocalDateTime horario;
+
+    private boolean confirmado;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public LocalDateTime getHorario() {
+        return horario;
+    }
+
+    public void setHorario(LocalDateTime horario) {
+        this.horario = horario;
+    }
+
+    public boolean isConfirmado() {
+        return confirmado;
+    }
+
+    public void setConfirmado(boolean confirmado) {
+        this.confirmado = confirmado;
+    }
+
+    // Getters e Setters
     
 }
