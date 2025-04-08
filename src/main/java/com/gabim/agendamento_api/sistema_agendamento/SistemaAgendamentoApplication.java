94996a2c2ae0a0922f0473f50061db8050d49174
gabimmdev <gabrielagomescezar@gmail.com>
2025-04-08 10:26:12 -0300

    first commit

---
+++ src/main/java/com/gabim/agendamento_api/sistema_agendamento/SistemaAgendamentoApplication.java
@@ -0,0 +1,13 @@
+package com.gabim.agendamento_api.sistema_agendamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAgendamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAgendamentoApplication.class, args);
+	}
+
+}
