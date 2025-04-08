0b76ef9c9530504fdd269dafd5b54c15e4ff9a62
gabimmdev <gabrielagomescezar@gmail.com>
2025-04-08 12:10:54 -0300

    feat: controller

--- src/main/java/com/gabim/agendamento_api/controller/AgendamentoController.java
+++ src/main/java/com/gabim/agendamento_api/controller/AgendamentoController.java
@@ -1,5 +1,38 @@
 package com.gabim.agendamento_api.controller;
 
+import com.gabim.agendamento_api.model.Agendamento;
+import com.gabim.agendamento_api.service.AgendamentoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/agendamentos")
+@CrossOrigin(origins = "*")
 public class AgendamentoController {
-    
+    private final AgendamentoService service;
+
+    public AgendamentoController(AgendamentoService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public Agendamento criar(@RequestBody Agendamento agendamento) {
+        return service.criarAgendamento(agendamento);
+    }
+
+    @GetMapping
+    public List<Agendamento> listar() {
+        return service.listarTodos();
+    }
+
+    @PutMapping("/{id}/confirmar")
+    public Agendamento confirmar(@PathVariable Long id) {
+        return service.confirmarAgendamento(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id) {
+        service.deletarAgendamento(id);
+    }
 }
