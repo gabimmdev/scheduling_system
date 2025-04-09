cc2aa8d653abe0df35be639f58e5f911631e1025
gabimmdev <gabrielagomescezar@gmail.com>
2025-04-09 12:03:55 -0300

    fix: imports and import repository

--- src/main/java/com/gabim/agendamento_api/controller/AgendamentoController.java
+++ src/main/java/com/gabim/agendamento_api/controller/AgendamentoController.java
@@ -2,15 +2,24 @@ package com.gabim.agendamento_api.controller;
 
 import com.gabim.agendamento_api.model.Agendamento;
 import com.gabim.agendamento_api.service.AgendamentoService;
+import com.gabim.agendamento_api.repository.AgendamentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/agendamentos")
+@Repository
 public class AgendamentoController {
     private final AgendamentoService service;
+    
+    @Autowired
+    private AgendamentoRepository agendamentoRepository; 
 
     public AgendamentoController(AgendamentoService service) {
         this.service = service;
@@ -31,7 +40,8 @@ public class AgendamentoController {
     public Agendamento confirmar(@PathVariable Long id) {
         return service.confirmarAgendamento(id);
     }
-     @PutMapping("/{id}/concluir")
+
+    @PutMapping("/{id}/concluir")
     public ResponseEntity<Agendamento> marcarComoConcluido(@PathVariable Long id) {
         Optional<Agendamento> optional = agendamentoRepository.findById(id);
 
@@ -44,7 +54,7 @@ public class AgendamentoController {
         agendamentoRepository.save(agendamento);
 
         return ResponseEntity.ok(agendamento);
-    }
+    } 
 
     @DeleteMapping("/{id}")
     public void deletar(@PathVariable Long id) {
