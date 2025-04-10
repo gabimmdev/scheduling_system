11c75b215c2d118d8223d3dfa3cee745dbbc340c
gabimmdev <gabrielagomescezar@gmail.com>
2025-04-09 21:25:13 -0300

    fix: AgendamentoSistemaApplication para diretorio raiz

--- src/main/java/com/gabim/agendamento_api/AgendamentoSistemaApplication.java
+++ src/main/java/com/gabim/agendamento_api/AgendamentoSistemaApplication.java
@@ -1,4 +1,4 @@
-package com.gabim.agendamento_api.sistema_agendamento;
+package com.gabim.agendamento_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
